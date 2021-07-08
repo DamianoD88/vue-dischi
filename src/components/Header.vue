48fe6f7a84888c498e83243159fe34bc299b78b1
DamianoD88 <dispe88@live.it>
2021-07-08 17:45:18 +0200

    add init

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <!-- <h1>{{ message }}</h1> -->
+      <img class="logo rounded-circle" src="../../img/download.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    // props: ["message"]
+}
+</script>
+
+<style>
+
+header {
+    width: 100%;
+    height: 70px;
+    background-color: #2e3a46;
+
+    .logo {
+        width: 40px;
+        margin: 15px;
+    }
+}
+
+</style>
